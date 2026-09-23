e8c3fa9ec2bcca67960db48f03f4660ad3042a66
HramchenkovViktor <harmchenkovviktor@gmail.com>
2026-09-23 14:06:57 +0300

    4

--- NASAApi/ViewController.swift
+++ NASAApi/ViewController.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private let titleLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupConstraints()
+        
         Task {
-              await loadData()
-          }
+            await loadData()
+        }
     }
+    
+    func setupUI() {
+        view.addSubview(titleLabel)
+        
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 0
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+    }
+    
+    func setupConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
     private func loadData() async {
         guard let url = URL(
             string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
@@ -32,8 +55,8 @@ class ViewController: UIViewController {
                 return
             }
             let nasaImage = try JSONDecoder().decode(NASAImage.self, from: data)
-            print(nasaImage.title)
-
+            await MainActor.run {  titleLabel.text = nasaImage.title }
+            
         } catch {
             print(error)
         }
