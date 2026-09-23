6e27589f015aa1165463e1ffa735dda2d8ebe230
HramchenkovViktor <harmchenkovviktor@gmail.com>
2026-09-23 13:34:52 +0300

    1

---
+++ NASAApi/NASAImage.swift
@@ -0,0 +1,13 @@
+//
+//  NASAImage.swift
+//  NASAApi
+//
+//  Created by Виктор on 23.09.2026.
+//
+
+struct NASAImage: Decodable {
+    let title: String
+    let date: String
+    let explanation: String
+    let url: String
+}
